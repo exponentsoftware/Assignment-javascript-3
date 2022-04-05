9053cc02d16cab98f93d5f6eb8a6d0e8ca7fcf22
sai0662 <sait0662@gmail.com>
2022-04-05 18:03:49 +0530

    converting array to objects

---
+++ 1a.js
@@ -0,0 +1,7 @@
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+]
+
+const student_data = {...students};
+console.log(student_data)
